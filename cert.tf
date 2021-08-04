da44bb6cc668fa045226aa559af6f0bf6b3b4e74
Zoltan Kovacs <>
2021-08-04 10:17:09 +1000

    check test-2

--- cert.tf
+++ cert.tf
@@ -11,10 +11,10 @@ data "aws_route53_zone" "example" {
 resource "aws_route53_record" "example" {
   for_each = {
   for dvo in aws_acm_certificate.my-cert.domain_validation_options : dvo.domain_name => {
-    name   = dvo.resource_record_name
+    name = dvo.resource_record_name
     record = dvo.resource_record_value
-    type   = dvo.resource_record_type
-  
+    type = dvo.resource_record_type
+  }
   }
 
   allow_overwrite = true
