a66f41fc3a13fdc15c272e3bf53e2437b6e04931
Zoltan Kovacs <>
2021-08-04 10:01:23 +1000

    check test

--- cert.tf
+++ cert.tf
@@ -8,13 +8,13 @@ data "aws_route53_zone" "example" {
   private_zone = false
 }
 
-resource "aws_route53_recor" "example" {
+resource "aws_route53_record" "example" {
   for_each = {
   for dvo in aws_acm_certificate.my-cert.domain_validation_options : dvo.domain_name => {
     name   = dvo.resource_record_name
     record = dvo.resource_record_value
     type   = dvo.resource_record_type
-  }
+  
   }
 
   allow_overwrite = true
